a00f3fc5194e065915248b54301dff9fcb77dc4d
jasonjurotich <jjir.org>
2023-08-12 16:38:31 -0600

    updated

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,8 +9,8 @@ vim.keymap.set('n', '<leader>fh', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- find files within github folder
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {}) -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 vim.keymap.set('n', '<leader>gfc', builtin.git_bcommits, {}) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-vim.keymap.set('n', '<leader>fh', builtin.git_branches, {})-- list git branches (use <cr> to checkout) ["gb" for git branch]
-vim.keymap.set('n', '<leader>fh', builtin.git_status, {}) -- list current changes per file with diff preview ["gs" for git status]
+vim.keymap.set('n', '<leader>go', builtin.git_branches, {})-- list git branches (use <cr> to checkout) ["gb" for git branch]
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {}) -- list current changes per file with diff preview ["gs" for git status]
 
 vim.keymap.set("n", "<leader>s", function()
 	require("telescope.builtin").spell_suggest(require("telescope.themes").get_cursor({}))
--- init.lua
+++ init.lua
@@ -1,1 +1,5 @@
 require("jjnvim")
+
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/605
+--  in order to see git diff with delta in telescope. 
