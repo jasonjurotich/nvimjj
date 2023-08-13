3d29ce73a3bedb7d04c8d79fb613698494b92f74
jasonjurotich <jjir.org>
2023-08-12 18:47:58 -0600

    updated

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -129,6 +129,7 @@ cmp.setup({
 		{ name = "luasnip" }, -- snippets
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
+		{ name = "cmp_tabnine" },
 		{ name = "lua-latex-symbols", option = { cache = true } },
 		{
 			name = "spell",
