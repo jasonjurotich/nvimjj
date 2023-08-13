53fb92a48603a83205b6062f0935a5f979c55816
jasonjurotich <jjir.org>
2023-08-12 18:15:32 -0600

    updated

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,22 +1,33 @@
 require("nvim-treesitter.configs").setup({
-  highlight = {
-    enable = true,
-  },
-  rainbow = { 
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
-  indent = { enable = false, disable = { "rust" } },
-  autotag = { enable = true },
-  ensure_installed = {
-    "json", "javascript",
-    "typescript", "tsx", "yaml",
-    "html", "css", "markdown",
-    "svelte", "graphql", "bash",
-    "lua", "vim", "dockerfile",
-    "gitignore", "rust", "latex",
-  },
-  auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+	indent = { enable = false, disable = { "rust" } },
+	autotag = { enable = true },
+	ensure_installed = {
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"markdown_inline",
+		"svelte",
+		"graphql",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+		"gitignore",
+		"rust",
+		"latex",
+	},
+	auto_install = true,
 })
-
