3bc0beac5122fadf1f05ca5cb89ffdb4ff85f604
jasonjurotich <jjir.org>
2023-08-18 11:13:08 -0600

    updated

--- after/plugin/hardline.lua
+++ after/plugin/hardline.lua
@@ -1,5 +1,4 @@
 require('hardline').setup {
-  bufferline = false,  -- disable bufferline
   theme = 'default',   -- change theme
   sections = {         -- define sections
     {class = 'mode', item = require('hardline.parts.mode').get_item},
@@ -12,24 +11,3 @@ require('hardline').setup {
     {class = 'warning', item = require('hardline.parts.whitespace').get_item},
   },
 }
-
-
--- local custom_vscode = require("lualine.themes.vscode")
-
--- custom_vscode.normal.c.bg = "None"
--- custom_vscode.insert.c.bg = "None"
--- custom_vscode.command.c.bg = "None"
---
--- require("lualine").setup({
--- 	options = {
--- 		icons_enabled = false,
--- 		theme = custom_vscode,
--- 		component_separators = "|",
--- 		section_separators = "",
---     sections = {
---       lualine_x = {},
---     },
--- 		path = 2,
--- 	}
--- })
-
