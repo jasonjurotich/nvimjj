8fb25a81726f9d48432b8a7a89a9596eaf4c9e51
jasonjurotich <jjir.org>
2023-08-19 08:53:53 -0600

    updated

--- after/plugin/hardline.lua
+++ after/plugin/hardline.lua
@@ -2,6 +2,7 @@ require('hardline').setup {
   theme = 'default',   -- change theme
   sections = {         -- define sections
     {class = 'mode', item = require('hardline.parts.mode').get_item},
+    {class = 'high', item = require('hardline.parts.git').get_item, hide = 100},
     {class = 'error', item = require('hardline.parts.lsp').get_error},
     {class = 'warning', item = require('hardline.parts.lsp').get_warning},
     {class = 'low', item = require('hardline.parts.wordcount').get_item, hide = 100},
