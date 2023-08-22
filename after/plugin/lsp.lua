0b8f82ed5f59ad42290417146f47fbf2d36e3aa9
jasonjurotich <jjir.org>
2023-08-21 20:06:56 -0600

    updated

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -141,3 +141,7 @@ require("rust-tools").setup({
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 	border = "rounded",
 })
+
+vim.diagnostic.config({
+	float = { border = "rounded" },
+})
