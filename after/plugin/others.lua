363a9b03faf4792977850d541935026e832dc0d6
jasonjurotich <jjir.org>
2023-09-02 17:32:15 -0600

    updated

--- after/plugin/others.lua
+++ after/plugin/others.lua
@@ -1,5 +1,19 @@
 require("gitsigns").setup()
-require("dressing").setup()
+
+vim.api.nvim_set_hl(0, "MyNormal", { bg = "None", fg = "White" })
+vim.api.nvim_set_hl(0, "MyNormalNC", { bg = "None", fg = "None" })
+vim.api.nvim_set_hl(0, "MyNonText", { bg = "None", fg = "None" })
+vim.api.nvim_set_hl(0, "MyNormalFloat", { bg = "None", fg = "None" })
+vim.api.nvim_set_hl(0, "MyFloatBorder", { bg = "None", fg = "#464140" })
+vim.api.nvim_set_hl(0, "MyCursorLine", { bg = "#837674", fg = "White" })
+
+require("dressing").setup({
+	input = {
+		win_options = {
+			winhighlight = "Normal:MyNormal,FloatBorder:MyFloatBorder,CursorLine:MyCursorLine,NormalFloat:MyNormalFloat,NormalNC:MyNormalNC,NonText:MyNonText,Search:None",
+		},
+	},
+})
 
 require("Comment").setup({
 	toggler = {
