68397539f6f00299f0840903dc4d9cc5e8e28c91
jasonjurotich <jjir.org>
2023-09-12 17:30:45 -0600

    updated

--- lua/jjnvim/init.lua
+++ lua/jjnvim/init.lua
@@ -3,3 +3,5 @@ require("jjnvim.opts")
 require("jjnvim.packer")
 
 vim.cmd([[hi FloatBorder guifg=darkgrey guibg=NONE]])
+vim.api.nvim_set_hl(0, "FidgetTitle", { link = "NormalFloat" })
+vim.api.nvim_set_hl(0, "FidgetTask", { link = "NormalFloat" })
