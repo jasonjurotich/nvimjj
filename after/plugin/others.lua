2fb2b938edd4691559740d55dd392f9920f4f4aa
jasonjurotich <jjir.org>
2023-09-28 07:43:06 -0600

    updated

--- after/plugin/others.lua
+++ after/plugin/others.lua
@@ -33,7 +33,7 @@ require("ibl").setup({
 	space_char_blankline = " ",
 	show_current_context = true,
 	show_current_context_start = true,
-	show_end_of_line = false,
+	show_end_of_line = true,
 })
 
 require("nvim-autopairs").setup({
