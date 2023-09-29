a2a5a71aa198fbe0a2751892b0d5d9b830ec021a
jasonjurotich <jjir.org>
2023-09-29 17:40:47 -0600

    updated

--- after/plugin/others.lua
+++ after/plugin/others.lua
@@ -27,14 +27,14 @@ require("Comment").setup({
 	},
 })
 
-require("ibl").setup({
-	buftype_exclude = { "terminal" },
-	filetype_exclude = { "dashboard", "packer" },
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-	show_end_of_line = true,
-})
+-- require("ibl").setup({
+-- 	buftype_exclude = { "terminal" },
+-- 	filetype_exclude = { "dashboard", "packer" },
+-- 	space_char_blankline = " ",
+-- 	show_current_context = true,
+-- 	show_current_context_start = true,
+-- 	show_end_of_line = true,
+-- })
 
 require("nvim-autopairs").setup({
 	check_ts = true, -- enable treesitter
