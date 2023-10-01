a7e3c422edb6f5e8f30f9d82ff83324cd76e62dc
jasonjurotich <jjir.org>
2023-09-30 18:47:17 -0600

    updated

--- after/plugin/linting.lua
+++ after/plugin/linting.lua
@@ -3,3 +3,11 @@ require("lint").linters_by_ft = {
 	javascript = { "eslint_d" },
 	cspell = { "cspell" },
 }
+
+local lint_augroup = vim.api.nvim_create_augroup("lint", {clear = true})
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+  group = lint_augroup,
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
