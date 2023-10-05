9d7cf3c36ff03806835fb34190bb52f011593a9a
jasonjurotich <jjir.org>
2023-10-04 19:30:52 -0600

    updated

--- lua/jjnvim/init.lua
+++ lua/jjnvim/init.lua
@@ -2,6 +2,6 @@ require("jjnvim.keymaps")
 require("jjnvim.opts")
 require("jjnvim.packer")
 
-vim.cmd([[hi FloatBorder guifg=darkgrey guibg=NONE]])
+vim.cmd([[hi FloatBorder guifg=#515151 guibg=NONE]])
 vim.api.nvim_set_hl(0, "FidgetTitle", { link = "NormalFloat" })
 vim.api.nvim_set_hl(0, "FidgetTask", { link = "NormalFloat" })
